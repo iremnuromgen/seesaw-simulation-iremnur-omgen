357bcd2abfaa57fea72be306fe3120361290fa93
irem <iremnuromgen@gmail.com>
2025-11-14 08:06:29 +0300

    feat: add object creation logic with random weight, size and color

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,5 @@
+import { createWeightObject } from "./objectManager.js";
+
 const seesawPlank =  document.getElementById('seesaw-plank');
 
 seesawPlank.addEventListener('click', (event) => {
@@ -5,4 +7,6 @@ seesawPlank.addEventListener('click', (event) => {
     const clickX = event.clientX - plankRect.left;
     const centerX = plankRect.width / 2;
     const distanceFromCenter = clickX - centerX;
+
+    if(clickX >= 0 && clickX <= plankRect.width) createWeightObject(seesawPlank, clickX, distanceFromCenter);
 });
---
+++ src/js/objectManager.js
@@ -0,0 +1,41 @@
+const weightColors = {
+    1: "#f94144",
+    2: "#f3722c",
+    3: "#f8961e",
+    4: "#f9844a",
+    5: "#f9c74f",
+    6: "#90be6d",
+    7: "#43aa8b",
+    8: "#4d908e",
+    9: "#577590",
+    10: "#277da1"
+};
+
+export function getRandomWeight() {
+    return Math.floor(Math.random() * 10) + 1;
+}
+
+export function createWeightObject(plank, clickX, distanceFromCenter) {
+    const boxWeight = getRandomWeight();
+
+    const box = document.createElement("div");
+
+    box.classList.add("weight-box")
+
+    const boxSize = 20 + boxWeight * 4;
+    box.style.width = `${boxSize}px`;
+    box.style.height = `${boxSize}px`;
+
+    box.style.backgroundColor = weightColors[boxWeight];
+
+    const left = clickX - boxSize / 2;
+    box.style.position = "absolute";
+
+    box.style.bottom = "10px";
+    box.style.left = `${left}px`;
+    box.style.borderRadius = "4px";
+
+    box.dataset.boxWeight = boxWeight;
+
+    plank.appendChild(box);
+}
